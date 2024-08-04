3151179c1644570aeb5997f6a71765221c34576b
Brendan O'Brien <bobrien@pursuit.org>
2024-08-04 16:07:09 -0400

    initializes object

--- src/Components/EditEntryForm.jsx
+++ src/Components/EditEntryForm.jsx
@@ -3,4 +3,20 @@ import { useNavigate, Link } from "react-router-dom"
 
 const API = import.meta.env.VITE_API_URL;
 
-export default function EditEntryForm ()
+export default function EditEntryForm () {
+    let { id } = useParams();
+    const navigate = useNavigate();
+
+    const [ entry, setEntry ] = useState [{
+        // initialize object
+        "date": '',
+        "time": '',
+        "last_meal": '',
+        "cabrs": 0,
+        "calories": 0,
+        "fat": 0,
+        "fiber": 0,
+        "glucose_gdl": 0,
+        "a1c": 0
+    }]
+}
