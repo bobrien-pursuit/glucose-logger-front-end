f4089edc8bbb914464a7f0a790b1f8470fe7cd07
Brendan O'Brien <bobrien@pursuit.org>
2024-08-05 12:13:25 -0400

    removes comments

--- src/Components/EditEntryForm.jsx
+++ src/Components/EditEntryForm.jsx
@@ -1,26 +1,3 @@
-// import { useState } from "React";
-// import { useNavigate, Link } from "react-router-dom"
-
-// const API = import.meta.env.VITE_API_URL;
-
-// export default function EditEntryForm () {
-//     let { id } = useParams();
-//     const navigate = useNavigate();
-
-//     const [ entry, setEntry ] = useState [{
-//         // initialize object
-//         "date": '',
-//         "time": '',
-//         "last_meal": '',
-//         "cabrs": 0,
-//         "calories": 0,
-//         "fat": 0,
-//         "fiber": 0,
-//         "glucose_gdl": 0,
-//         "a1c": 0
-//     }]
-// }
-
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
